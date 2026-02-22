0b1836e09b5f2f285070189db08df8835f6909d2
Young Seo Kim <youngseokim7@gmail.com>
2026-02-22 16:36:56 +0900

    run wrappers for prompting / running from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use std::{
+    env::{self},
+    fs, io,
+};
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let length = args.len();
+    if length > 1 {
+        print!("USAGE: ./binary /path/to/file \n OR \n cargo run -- path/to/file")
+    } else if length == 1 {
+        run_from_file(args.get(1).unwrap()).unwrap_or_else(|err| {
+            panic!("failed to run from file: {:?}", err);
+        });
+    } else {
+        run_by_prompt().unwrap_or_else(|err| {
+            panic!("failed to run interactive prompt: {:?}", err);
+        });
+    }
     println!("Hello, world!");
+}
+
+fn run_from_file(path: &str) -> io::Result<()> {
+    let content = fs::read_to_string(path)?;
+    run(content);
+    Ok(())
+}
+
+fn run_by_prompt() -> io::Result<()> {
+    loop {
+        print!("> ");
+
+        let mut line = String::new();
+        let bytes_read = io::stdin().read_line(&mut line)?;
+
+        if bytes_read == 0 {
+            print!("");
+            break;
+        }
+
+        run(line);
+    }
+
+    Ok(())
+}
+
+fn run(source: String) {
+    todo!("scanner + tokens must be implemented in run");
 }
