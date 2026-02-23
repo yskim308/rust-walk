cdd2c8f109dbc10caaeb301df8caa01659f6dd67
Young Seo Kim <youngseokim7@gmail.com>
2026-02-23 14:50:25 +0900

    move errors into its own lib

---
+++ src/error.rs
@@ -0,0 +1,14 @@
+pub struct LoxError {
+    line: usize,
+    message: String,
+}
+
+impl LoxError {
+    pub fn new(line: usize, message: String) -> Self {
+        LoxError { line, message }
+    }
+
+    pub fn report(&self) {
+        eprint!("[line {}] Error {}", self.line, self.message)
+    }
+}
