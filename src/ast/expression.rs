f3103b6c5dea038d511879aadf45e71cca83f859
Young Seo Kim <youngseokim7@gmail.com>
2026-02-24 16:01:39 +0900

    pretty printing Expr enums

--- src/ast/expression.rs
+++ src/ast/expression.rs
@@ -1,4 +1,4 @@
-use crate::scanner::token_type::TokenType;
+use crate::scanner::token::Token;
 
 pub enum Expr {
     Literal {
@@ -8,12 +8,12 @@ pub enum Expr {
         expression: Box<Expr>,
     },
     Unary {
-        token: TokenType,
+        token: Token,
         expression: Box<Expr>,
     },
     Binary {
         left_expr: Box<Expr>,
-        operator: TokenType,
+        operator: Token,
         right_expr: Box<Expr>,
     },
 }
@@ -23,4 +23,33 @@ pub enum LiteralValue {
     String(String),
     Boolean(bool),
     Nil,
+}
+
+impl Expr {
+    pub fn pretty_print(&self) -> String {
+        match self {
+            Expr::Literal { value } => match value {
+                LiteralValue::Number(n) => n.to_string(),
+                LiteralValue::String(s) => s.clone(),
+                LiteralValue::Boolean(b) => b.to_string(),
+                LiteralValue::Nil => "Nil".to_string(),
+            },
+            Expr::Grouping { expression } => expression.pretty_print(),
+            Expr::Unary { token, expression } => {
+                format!("{} {}", token.lexeme, expression.pretty_print())
+            }
+            Expr::Binary {
+                left_expr,
+                operator,
+                right_expr,
+            } => {
+                format!(
+                    "{} {} {}",
+                    left_expr.pretty_print(),
+                    operator.lexeme,
+                    right_expr.pretty_print()
+                )
+            }
+        }
+    }
 }
