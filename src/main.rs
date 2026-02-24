5f09a1c34e52207403a18a37311dd4c84cbd07c6
Young Seo Kim <youngseokim7@gmail.com>
2026-02-24 16:25:59 +0900

    add pretty print check in main

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,19 @@ use std::{
     fs, io,
 };
 
+use rlox::{
+    ast::expression::{Expr, LiteralValue},
+    scanner::{token::Token, token_type::TokenType},
+};
+
 fn main() {
+    println!("Hello, world!");
+    check_pretty_print();
     let args: Vec<String> = env::args().collect();
     let length = args.len();
-    if length > 1 {
+    if length > 2 {
         print!("USAGE: ./binary /path/to/file \n OR \n cargo run -- path/to/file")
-    } else if length == 1 {
+    } else if length == 2 {
         run_from_file(args.get(1).unwrap()).unwrap_or_else(|err| {
             panic!("failed to run from file: {:?}", err);
         });
@@ -17,7 +24,6 @@ fn main() {
             panic!("failed to run interactive prompt: {:?}", err);
         });
     }
-    println!("Hello, world!");
 }
 
 fn run_from_file(path: &str) -> io::Result<()> {
@@ -44,6 +50,24 @@ fn run_by_prompt() -> io::Result<()> {
     Ok(())
 }
 
-fn run(source: String) {
+fn run(_source: String) {
     todo!("scanner + tokens must be implemented in run");
+}
+
+fn check_pretty_print() {
+    let expr = Expr::Binary {
+        left_expr: Box::new(Expr::Unary {
+            token: Token::new(TokenType::Minus, "-".to_string(), None, 1),
+            expression: Box::new(Expr::Literal {
+                value: LiteralValue::Number(123.),
+            }),
+        }),
+        operator: Token::new(TokenType::Star, "*".to_string(), None, 1),
+        right_expr: Box::new(Expr::Grouping {
+            expression: Box::new(Expr::Literal {
+                value: LiteralValue::Number(45.67),
+            }),
+        }),
+    };
+    println!("{expr}");
 }
