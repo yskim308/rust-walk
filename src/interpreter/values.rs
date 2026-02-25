f8786579f2d4de919ed7ffd309cfd3e5600de596
Young Seo Kim <youngseokim7@gmail.com>
2026-02-25 22:51:10 +0900

    implement helper functions for extracting wrapped values

--- src/interpreter/values.rs
+++ src/interpreter/values.rs
@@ -16,3 +16,24 @@ impl std::fmt::Display for Value {
         }
     }
 }
+
+impl Value {
+    pub fn is_truthy(&self) -> bool {
+        match *self {
+            Value::Nil => false,
+            Value::Boolean(b) => b,
+            _ => true,
+        }
+    }
+
+    pub fn is_numeric(&self) -> bool {
+        matches!(self, Value::Number(_))
+    }
+
+    pub fn as_number(&self) -> f64 {
+        match *self {
+            Value::Number(n) => n,
+            _ => panic!("as number called on non number type: {:?}", self),
+        }
+    }
+}
