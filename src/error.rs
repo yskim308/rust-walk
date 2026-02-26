367a5b23249249033a1e14d474cd2eda8388c5bb
Young Seo Kim <youngseokim7@gmail.com>
2026-02-26 14:42:36 +0900

    create error type for runtime (interpreter)

--- src/error.rs
+++ src/error.rs
@@ -1,19 +1,47 @@
 use std::fmt::{write, Display};
 
+use crate::scanner::token::Token;
+
+#[derive(Debug, Clone)]
+pub enum ErrorKind {
+    Static,
+    Runtime,
+}
+
 #[derive(Debug, Clone)]
 pub struct LoxError {
     line: usize,
     message: String,
+    kind: ErrorKind,
 }
 
 impl LoxError {
     pub fn new(line: usize, message: String) -> Self {
-        LoxError { line, message }
+        LoxError {
+            line,
+            message,
+            kind: ErrorKind::Static,
+        }
+    }
+
+    pub fn runtime(token: Token, message: String) -> Self {
+        LoxError {
+            line: token.line,
+            message,
+            kind: ErrorKind::Runtime,
+        }
     }
 }
 
 impl Display for LoxError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "[line {}] Error {}", self.line, self.message)
+        match self.kind {
+            ErrorKind::Static => {
+                write!(f, "Static Error on [line {}]: {}", self.line, self.message)
+            }
+            ErrorKind::Runtime => {
+                write!(f, "Runtime Error on [line {}:] {}", self.line, self.message)
+            }
+        }
     }
 }
