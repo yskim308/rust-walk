9f25f9c58557a3e2c3d71b0da84a2f207e7df840
Young Seo Kim <youngseokim7@gmail.com>
2026-03-01 10:02:27 +0900

    add recursive checks in environment

--- src/interpreter/environment.rs
+++ src/interpreter/environment.rs
@@ -3,12 +3,14 @@ use std::collections::HashMap;
 use crate::{error::LoxError, interpreter::values::Value, scanner::token::Token};
 
 pub struct Environment {
+    environment: Option<Box<Environment>>,
     values: HashMap<String, Value>,
 }
 
 impl Environment {
     pub fn new() -> Self {
         Environment {
+            environment: None,
             values: HashMap::new(),
         }
     }
@@ -18,6 +20,10 @@ impl Environment {
     }
 
     pub fn get(&self, name: &Token) -> Result<Value, LoxError> {
+        if let Some(env) = &self.environment {
+            return env.get(name);
+        }
+
         match self.values.get(&name.lexeme) {
             Some(value) => Ok(value.clone()),
             None => Err(LoxError::new(
@@ -28,6 +34,10 @@ impl Environment {
     }
 
     pub fn assign(&mut self, left: Token, right: &Value) -> Result<(), LoxError> {
+        if let Some(env) = &mut self.environment {
+            return env.assign(left, right);
+        }
+
         if let Some(key) = self.values.get_mut(&left.lexeme) {
             *key = right.clone();
             Ok(())
