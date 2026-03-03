c5e6f1ea341e5c7f2b9963df53432ed5471d5596
Young Seo Kim <youngseokim7@gmail.com>
2026-03-04 08:34:50 +0900

    add if statement to Stmt enum

--- src/interpreter/stmt.rs
+++ src/interpreter/stmt.rs
@@ -4,6 +4,24 @@ use crate::{ast::expression::Expr, scanner::token::Token};
 pub enum Stmt {
     Block(Vec<Stmt>),
     Expression(Expr),
+    If(IfConditions),
     Print(Expr),
     Var(Token, Option<Expr>), // variables can be delcared unitialized
+}
+
+#[derive(Debug)]
+pub struct IfConditions {
+    pub condition: Expr,
+    pub then_branch: Box<Stmt>,
+    pub else_branch: Box<Stmt>,
+}
+
+impl IfConditions {
+    pub fn new(condition: Expr, then_branch: Stmt, else_branch: Stmt) -> Self {
+        IfConditions {
+            condition,
+            then_branch: Box::new(then_branch),
+            else_branch: Box::new(else_branch),
+        }
+    }
 }
